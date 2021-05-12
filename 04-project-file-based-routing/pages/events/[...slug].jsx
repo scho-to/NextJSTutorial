6eeea583344d8b5bfe91054c4d09a16a93f08e12
Tobias Schotenröhr <tobias.schotenroehr@gmail.com>
2021-05-12 13:56:41 +0200

    Section 5 created

--- 04-project-file-based-routing/pages/events/[...slug].jsx
+++ 04-project-file-based-routing/pages/events/[...slug].jsx
@@ -1,9 +1,51 @@
-export default function filteredEventPage() {
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import ResultsTitle from '../../components/events/results-title';
+import ErrorAlert from '../../components/ui/error-alert';
+import Button from '../../components/ui/button';
+
+export default function filteredEventPage(props) {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  let date = null;
+  let filteredYear = null;
+  let filteredMonth = null;
+  let events = [];
+  
+  if (!filterData) {
+    return <p className='center'>Loading...</p>
+  }
+  filteredYear = +filterData[0];
+  filteredMonth = +filterData[1];
+  
+  events = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth
+  });
+  
+  if (events.length === 0){
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p className='center'>Nothing Found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+  
+  date = new Date(filteredYear, filteredMonth-1);
+  
   return (
-    <div>
-      <h1>
-        filteredEventPage
-      </h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={events} />
+    </Fragment>
   );
 };
--- 04-project-file-based-routing/pages/events/[id].jsx
+++ 04-project-file-based-routing/pages/events/[id].jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { getEventById } from '../../dummy-data';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
 
 export default function EventDetailPage() {
   const router = useRouter();
@@ -11,7 +12,11 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event found!</p>
+    return (
+      <ErrorAlert>
+        <p>No Event found!</p>
+      </ErrorAlert>
+    )
   }
 
   return (
