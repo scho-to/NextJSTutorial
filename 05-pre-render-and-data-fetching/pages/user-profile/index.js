0302606684ee7e94278a9a2caaf37e0fd3e612b2
Tobias Schotenroehr <tobias.schotenroehr@gmail.com>
2021-06-16 15:16:09 +0200

    Checkin 16.06.2021

---
+++ 05-pre-render-and-data-fetching/pages/user-profile/index.js
@@ -0,0 +1,17 @@
+export default function UserProfilePage(props) {
+
+
+  return (
+    <h1>{props.username}</h1>
+  )
+};
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  return {
+    props: {
+      username: "Max"
+    }
+  }
+}
