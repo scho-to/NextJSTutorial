521b5e6e5bbd955632d9e7f966a848a77dd5210a
Schotenroehr <schotenroehr@dsv-saaten.de>
2021-11-05 14:19:26 +0100

    Project 11 continued.

--- 11-complete-blog/components/hero.js
+++ 11-complete-blog/components/hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <section className={classes.hero}>
             <div className={classes.image}>
-                <Image src="" alt="An image showing" />
+                <Image src="" alt="An image showing me" width height />
             </div>
             <h1>Hi, I'm Jon</h1>
             <p>I blog about web development - especially frontend frameworks</p>
